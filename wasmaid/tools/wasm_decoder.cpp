6b43a30381a0e4e5a780d5df8883fe6b395c0817
lipingan <lipingan.dev@outlook.com>
2022-05-21 16:49:45 +0800

    Add tools directory

---
+++ wasmaid/tools/wasm_decoder.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file wasm_decoder.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-05-21
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "wasm_decoder.h"
+namespace wasmaid {
+WasmDecoder::WasmDecoder() {}
+
+WasmDecoder::~WasmDecoder() {}
+} // namespace wasmaid
---
+++ wasmaid/tools/wasm_decoder.h
@@ -0,0 +1,26 @@
+/**
+ * @file wasm_decoder.h
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-05-21
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef __WASMAID_TOOLS_WASM_DECODER_H__
+#define __WASMAID_TOOLS_WASM_DECODER_H__
+namespace wasmaid {
+class WasmDecoder {
+
+public:
+  WasmDecoder();
+  ~WasmDecoder();
+
+private:
+};
+
+} // namespace wasmaid
+
+#endif
