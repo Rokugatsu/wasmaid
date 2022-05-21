5dc179a58f0bd14072de8492eecd3ae1435eab17
lipingan <lipingan.dev@outlook.com>
2022-05-21 16:50:16 +0800

    Add stack machine directory

---
+++ wasmaid/stack_machine/stack_machine.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file stack_machine.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-05-21
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "stack_machine.h"
+
+namespace wasmaid {
+StackMachine::StackMachine() {}
+
+StackMachine::~StackMachine() {}
+} // namespace wasmaid
---
+++ wasmaid/stack_machine/stack_machine.h
@@ -0,0 +1,25 @@
+/**
+ * @file stack_machine.h
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-05-21
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef __WASMAID_STACK_MACHINE_STACK_MACHINE_H__
+#define __WASMAID_STACK_MACHINE_STACK_MACHINE_H__
+namespace wasmaid {
+class StackMachine {
+public:
+  StackMachine();
+  ~StackMachine();
+
+private:
+};
+
+} // namespace wasmaid
+
+#endif
